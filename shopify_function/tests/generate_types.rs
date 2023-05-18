5c4728b374214e105b6fcf673e920577ff8069c5
Andrew Hassan <andrewihassan@gmail.com>
2023-05-18 15:28:27 -0400

    Split integration tests

--- shopify_function/tests/generate_types.rs
+++ shopify_function/tests/generate_types.rs
@@ -1,6 +1,4 @@
-use serde::Serialize;
 use shopify_function::prelude::*;
-use shopify_function::Result;
 
 generate_types!(
     query_path = "./tests/fixtures/input.graphql",
@@ -20,29 +18,4 @@ fn test_json_deserialization() {
     assert_eq!(parsed.id, "gid://shopify/Order/1234567890");
     assert_eq!(parsed.num, Some(123));
     assert_eq!(parsed.name, Some("test".to_string()));
-}
-
-const FUNCTION_INPUT: &str = r#"{
-    "id": "gid://shopify/Order/1234567890",
-    "num": 123,
-    "name": "test"
-}"#;
-static mut FUNCTION_OUTPUT: Vec<u8> = vec![];
-
-#[test]
-fn test_function() {
-    let expected_result = r#"{"name":"new name: gid://shopify/Order/1234567890"}"#;
-    main().unwrap();
-    let actual_result = std::str::from_utf8(unsafe { FUNCTION_OUTPUT.as_slice() }).unwrap();
-    assert_eq!(actual_result, expected_result);
-}
-
-#[shopify_function(
-    input_stream = std::io::Cursor::new(FUNCTION_INPUT.as_bytes().to_vec()),
-    output_stream = unsafe { &mut FUNCTION_OUTPUT }
-)]
-fn my_function(input: input::ResponseData) -> Result<output::FunctionResult> {
-    Ok(output::FunctionResult {
-        name: Some(format!("new name: {}", input.id)),
-    })
 }
